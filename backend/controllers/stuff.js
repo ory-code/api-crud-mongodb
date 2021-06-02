be96668a57ae16d1246cc873a8b5b1ed54c8bd46
Sacha Ory <sachaory@gmail.com>
2021-06-02 23:16:35 +0200

    controlleur init

---
+++ backend/controllers/stuff.js
@@ -0,0 +1,90 @@
+const Thing = require("../models/thing");
+
+exports.createThing = (req, res, next) => {
+  const thing = new Thing({
+    name: req.body.name,
+    manufacturer: req.body.manufacturer,
+    description: req.body.description,
+    mainPepper: req.body.mainPepper,
+    imageUrl: req.body.imageUrl,
+    heat: req.body.heat,
+    likes: req.body.likes,
+    dislikes: req.body.dislikes,
+    usersLiked: req.body.usersLiked,
+    usersDisliked: req.body.usersDisliked,
+    userId: req.body.userId,
+  });
+  thing
+    .save()
+    .then(() => {
+      res.status(201).json({
+        message: "Post saved successfully!",
+      });
+    })
+    .catch((error) => {
+      res.status(400).json({
+        error: error,
+      });
+    });
+};
+
+exports.getOneThing = (req, res, next) => {
+  Thing.findOne({
+    _id: req.params.id,
+  })
+    .then((thing) => {
+      res.status(200).json(thing);
+    })
+    .catch((error) => {
+      res.status(404).json({
+        error: error,
+      });
+    });
+};
+exports.modifyThing = (req, res, next) => {
+  const thing = new Thing({
+    _id: req.params.id,
+    title: req.body.title,
+    description: req.body.description,
+    imageUrl: req.body.imageUrl,
+    price: req.body.price,
+    userId: req.body.userId,
+  });
+  Thing.updateOne({ _id: req.params.id }, thing)
+    .then(() => {
+      res.status(201).json({
+        message: "Thing updated successfully!",
+      });
+    })
+    .catch((error) => {
+      res.status(400).json({
+        error: error,
+      });
+    });
+};
+
+exports.deleteThing = (req, res, next) => {
+  Thing.deleteOne({ _id: req.params.id })
+    .then(() => {
+      res.status(200).json({
+        message: "Deleted!",
+      });
+    })
+    .catch((error) => {
+      res.status(400).json({
+        error: error,
+      });
+    });
+};
+
+exports.getAllStuff = (req, res, next) => {
+  Thing.find()
+    .then((things) => {
+      res.status(200).json(things);
+    })
+    .catch((error) => {
+      res.status(400).json({
+        error: error,
+      });
+    });
+};
