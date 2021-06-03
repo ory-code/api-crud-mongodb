7839d680f63f6b4e5e925a2e1c8f1f05eab97d37
Sacha Ory <sachaory@gmail.com>
2021-06-03 20:52:48 +0200

    routes auth user

---
+++ backend/routes/users.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+const userCtrl = require("../controllers/user");
+
+router.post("/api/auth/signup", userCtrl.signup);
+router.post("/api/auth/login", userCtrl.login);
+
+module.exports = router;
