507549ed0920e24db4c1e83ace2be0db414deaa9
Sacha Ory <sachaory@gmail.com>
2021-06-04 18:56:00 +0200

    beautifyle file

--- backend/models/thing.js
+++ backend/models/thing.js
@@ -1,14 +1,42 @@
 const mongoose = require("mongoose");
 
 const thingShema = mongoose.Schema({
-  name: { type: String, required: true },
-  manufacturer: { type: String, required: true },
-  description: { type: String, required: true },
-  mainPepper: { type: String, required: true },
-  imageUrl: { type: String, required: true },
-  heat: { type: Number, required: true },
-  likes: { type: Number, required: true },
-  dislikes: { type: Number, required: true },
+  userId: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  manufacturer: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  mainPepper: {
+    type: String,
+    required: true,
+  },
+  imageUrl: {
+    type: String,
+    required: true,
+  },
+  heat: {
+    type: Number,
+    required: true,
+  },
+  likes: {
+    type: Number,
+    required: true,
+  },
+  dislikes: {
+    type: Number,
+    required: true,
+  },
   usersLiked: [String],
   usersDisliked: [String],
 });
