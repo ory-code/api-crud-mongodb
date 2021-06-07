48784f7bdacd8ac866e91f4424704d4c2cb39244
Sacha Ory <sachaory@gmail.com>
2021-06-07 20:59:49 +0200

    modif mineur

--- backend/routes/stuff.js
+++ backend/routes/stuff.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const stuffCtrl = require("../controllers/stuff");
+const stuffCtrl = require("../controllers/sauce");
 const auth = require("../middleware/auth");
 const multer = require("../middleware/multer-config");
 
