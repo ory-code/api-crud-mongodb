2aba2fe9d9c1f76c4ef4e86b51aa7b2908e32c79
Sacha Ory <sachaory@gmail.com>
2021-06-08 11:25:24 +0200

    saucelike

--- backend/routes/stuff.js
+++ backend/routes/stuff.js
@@ -3,12 +3,13 @@ const router = express.Router();
 const stuffCtrl = require("../controllers/sauce");
 const auth = require("../middleware/auth");
 const multer = require("../middleware/multer-config");
+const { route } = require("./users");
 
 router.get("/", auth, stuffCtrl.getAllStuff);
 router.post("/", auth, multer, stuffCtrl.createThing);
+router.post("/:id/like", auth, multer, stuffCtrl.likeSauce);
 router.get("/:id", auth, stuffCtrl.getOneThing);
-router.put("/:id", auth, multer,stuffCtrl.modifyThing);
+router.put("/:id", auth, multer, stuffCtrl.modifyThing);
 router.delete("/:id", auth, stuffCtrl.deleteThing);
-
 
 module.exports = router;
