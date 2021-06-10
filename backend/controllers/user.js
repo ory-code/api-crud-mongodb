17297ea08fc0b00c7bb02052ad279bf3351ade36
Sacha Ory <sachaory@gmail.com>
2021-06-10 21:23:23 +0200

    correction user fonction login et signup

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -2,77 +2,44 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const User = require("../models/user");
 
+
+
 exports.signup = (req, res, next) => {
-  bcrypt
-    .hash(req.body.password, 10)
-    .then((hash) => {
+  bcrypt.hash(req.body.password, 10)
+    .then(hash => {
       const user = new User({
         email: req.body.email,
-        password: hash,
+        password: hash
       });
-      user
-        .save()
-        .then(
-          res.status(201).json({
-            message: " user create !",
-          })
-        )
-        .catch((error) =>
-          res.status(400).json({
-            error,
-          })
-        );
+      user.save()
+        .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
+        .catch(error => res.status(400).json({ error }));
     })
-    .catch((error) =>
-      res.status(500).json({
-        error,
-      })
-    );
+    .catch(error => res.status(500).json({ error }));
 };
 
+
 exports.login = (req, res, next) => {
-  User.findOne({
-    email: req.body.email,
-  })
-    .then((user) => {
-      console.log(user, req.body.email);
+  User.findOne({ email: req.body.email })
+    .then(user => {
       if (!user) {
-        return res.status(401).json({
-          error: "user not find !",
-        });
+        return res.status(401).json({ error: 'Utilisateur non trouvé !' });
       }
-      const toto =  bcrypt.compareSync(req.body.password, user.password);
-      console.log(toto);
-      bcrypt
-        .compare(req.body.password, user.password)
-        .then((valid) => {
+      bcrypt.compare(req.body.password, user.password)
+        .then(valid => {
           if (!valid) {
-            return res.status(401).json({
-              error: "identifiant incorrect",
-            });
+            return res.status(401).json({ error: 'Mot de passe incorrect !' });
           }
           res.status(200).json({
             userId: user._id,
             token: jwt.sign(
-              {
-                userId: user._id,
-              },
-              "RANDOM_TOKEN_SECRET",
-              {
-                expiresIn: "24h",
-              }
-            ),
+              { userId: user._id },
+              'RANDOM_TOKEN_SECRET',
+              { expiresIn: '24h' }
+            )
           });
         })
-        .catch((error) =>
-          res.status(500).json({
-            error,
-          })
-        );
+        .catch(error => res.status(500).json({ error }));
     })
-    .catch((error) =>
-      res.status(500).json({
-        error,
-      })
-    );
+    .catch(error => res.status(500).json({ error }));
 };
