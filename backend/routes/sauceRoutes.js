f60f6839a7db374cba470ed69b5d58d4fb6a9f0e
Sacha Ory <sachaory@gmail.com>
2021-06-28 20:51:35 +0200

    modif nom variable

--- backend/routes/sauceRoutes.js
+++ backend/routes/sauceRoutes.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const stuffCtrl = require("../controllers/sauce");
+const sauceCtrl = require("../controllers/sauce");
 const auth = require("../middleware/auth");
 const multer = require("../middleware/multer-config");
 
 
-router.get("/", auth, stuffCtrl.getAllSauce);
-router.post("/", auth, multer, stuffCtrl.createSauce);
-router.post("/:id/like", auth, multer, stuffCtrl.likeSauce);
-router.get("/:id", auth, stuffCtrl.getOneSauce);
-router.put("/:id", auth, multer, stuffCtrl.modifySauce);
-router.delete("/:id", auth, stuffCtrl.deleteSauce);
+router.get("/", auth, sauceCtrl.getAllSauce);
+router.post("/", auth, multer, sauceCtrl.createSauce);
+router.post("/:id/like", auth, multer, sauceCtrl.likeSauce);
+router.get("/:id", auth, sauceCtrl.getOneSauce);
+router.put("/:id", auth, multer, sauceCtrl.modifySauce);
+router.delete("/:id", auth, sauceCtrl.deleteSauce);
 
 module.exports = router;
